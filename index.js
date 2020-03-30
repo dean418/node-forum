5aee3348cb4dc047f75274439e4fee78e92b2fad
dean808 <deansingleton1999@gmail.com>
2020-03-30 23:39:05 +0100

    added loaders and express session

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-const hbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const path = require('path');
+const loaders = require('./loaders/init');
 const express = require('express');
-
-const app = express();
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://dean:${DB_PASS}@cluster0-aeu8n.mongodb.net/test?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const startServer = async () => {
+    const app = express();
 
-const index = require('./routes/index');
+    await loaders.init(app);
 
-app.use(express.static(path.join(__dirname + 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+    app.listen(process.env.PORT, (err) => {
+        console.log(`server listening on port: ${process.env.PORT}`);
+    });
+}
 
-app.engine('.hbs', hbs({
-    defaultLayout: 'layout',
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
-
-app.use('/', index);
-
-app.listen(3000, () => {
-    console.log('listening on port 3000');
-});
+startServer();
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     res.render('index');
 });
 

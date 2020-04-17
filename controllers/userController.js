a196f6abfab14278d71e9f13703d08f3b18ddfd0
Code Nation <deansingleton1999@gmail.com>
2020-04-17 10:25:43 +0100

    fixed issue with userName being undefined

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,7 +29,7 @@ exports.create = async (req, res) => {
 	user.save();
 
 	req.session.userID = user._id;
-	req.session.userName = userName;
+	req.session.userName = user.userName;
 	req.session.save();
 
 	res.statusCode = 201;
@@ -56,7 +56,7 @@ exports.postLogin = async (req, res) => {
 
 
 	req.session.userID = user._id;
-	req.session.userName = userName;
+	req.session.userName = user.userName;
 	req.session.save();
 
 	res.status(200).redirect('/');
